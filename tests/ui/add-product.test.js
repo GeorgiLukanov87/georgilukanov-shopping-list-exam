d88b48874a4c9c8c3363ed39f8967900c4f85a87
Georgi St. Lukanov <goshiii87@gmail.com>
2024-04-02 15:05:53 +0300

    fix urls

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('https://georgilukanov-shopping-list-exam.onrender.com//Add-Product');
+    await page.goto('https://georgilukanov-shopping-list-exam.onrender.com/Add-Product');
     const form = await page.$('input');
     expect(form).toBeTruthy();
   });
